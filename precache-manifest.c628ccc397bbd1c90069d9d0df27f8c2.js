d92a4f3b28e92b76ded52e90426bbae5cd4f2c74
Fred Silveira <fredericos@eu.square-enix.com>
2020-06-27 23:25:41 +0100

    Updates

---
+++ precache-manifest.c628ccc397bbd1c90069d9d0df27f8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f0a961bbe5e07a20734275050ef78f",
+    "url": "/react-showcase/index.html"
+  },
+  {
+    "revision": "57b11cda42bfe0a723be",
+    "url": "/react-showcase/static/css/main.ebbf3f8b.chunk.css"
+  },
+  {
+    "revision": "fd47aaac62036ec756db",
+    "url": "/react-showcase/static/js/2.9585e401.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-showcase/static/js/2.9585e401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b11cda42bfe0a723be",
+    "url": "/react-showcase/static/js/main.da6113a9.chunk.js"
+  },
+  {
+    "revision": "23edf1d01dc21a030251",
+    "url": "/react-showcase/static/js/runtime-main.bf49687a.js"
+  }
+]);
